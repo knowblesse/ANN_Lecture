91f2dcf0f5d965749c11cf50b46af2d1fc968ec5
Knowblesse <knowblesse@gmail.com>
2018-05-21 21:17:20 +0900

    팔꿈치 추가

--- HW6.py
+++ HW6.py
@@ -300,3 +300,16 @@ plt.scatter(X[labels[:,0]>0.4,0],X[labels[:,0]>0.4,1],c='r')
 plt.scatter(X[labels[:,1]>0.4,0],X[labels[:,1]>0.4,1],c='g')
 plt.scatter(X[labels[:,2]>0.4,0],X[labels[:,2]>0.4,1],c='b')
 plt.show()
+
+
+distortions = []
+for i in range(1, 11):
+km = KMeans(n_clusters=i, init='k-means++', n_init=10, max_iter=300, random_state=0)
+km.fit(X)
+distortions.append(km.inertia_)
+plt.plot(range(1, 11), distortions, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distortion')
+plt.tight_layout()
+#plt.savefig('images/11_03.png', dpi=300)
+plt.show()
